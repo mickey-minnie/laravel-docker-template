9b98fe98a03879422132c8276919b6b9f8bb879e
mickey-minnie <hina.hina000ftr0z0@gmail.com>
2024-05-14 11:31:47 +0900

    一覧表示のデータ取得

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        // 追加
+        $todo = new Todo();
+        $todoList = $todo->all();
+        dd($todoList);
+
         return view('todo.index');
     }
 }
